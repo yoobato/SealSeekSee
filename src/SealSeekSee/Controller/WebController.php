579df3753040e43ea2479a60ede9e46b0235fe06
Daeyeol Ryu <dy@yoobato.com>
2017-06-01 22:17:38 +0900

    what 3 words api util 추가

--- src/SealSeekSee/Controller/WebController.php
+++ src/SealSeekSee/Controller/WebController.php
@@ -1,8 +1,10 @@
 <?php
 namespace SealSeekSee\Controller;
 
+use SealSeekSee\Util\What3WordsUtil;
 use Silex\Application;
 use Silex\ControllerProviderInterface;
+use Symfony\Component\HttpFoundation\Request;
 
 class WebController implements ControllerProviderInterface
 {
@@ -13,10 +15,17 @@ class WebController implements ControllerProviderInterface
          */
         $web = $app['controllers_factory'];
 
-        $web->get('/', function() use ($app) {
-            return $app['twig']->render('/index.twig');
-        });
+        $web->get('/', array($this, 'index'));
 
         return $web;
+    }
+
+    public function index(Request $req, Application $app)
+    {
+        // TODO: 테스트 코드
+        $coordinates = What3WordsUtil::address2Coordinates('index.home.raft');
+        $address = What3WordsUtil::coordinates2Address(51.521251,-0.203586);
+
+        return $app['twig']->render('/index.twig', array('coordinates' => $coordinates, 'address' => $address));
     }
 }
---
+++ src/SealSeekSee/Util/What3WordsUtil.php
@@ -0,0 +1,64 @@
+<?php
+namespace SealSeekSee\Util;
+
+// 참고 https://docs.what3words.com/api/v2/
+class What3WordsUtil
+{
+    const WHAT_3_WORDS_API_KEY = '1MPXZL99';
+
+    /**
+     * What 3 Words API (Forward Geocoding)
+     * 3단어 주소 => 위도, 경도
+     *
+     * @param $address
+     * @return array
+     */
+    public static function address2Coordinates($address)
+    {
+        $query = http_build_query([
+            'addr' => $address,
+            'display' => 'minimal',
+            'key' => static::WHAT_3_WORDS_API_KEY
+        ]);
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, 'https://api.what3words.com/v2/forward?' . $query);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+
+        $response = curl_exec($ch);
+        $response = json_decode($response, true);
+
+        return [
+            'lat' => $response['geometry']['lat'],
+            'lng' => $response['geometry']['lng']
+        ];
+    }
+
+    /**
+     * What 3 Words API (Reverse Geocoding)
+     * 위도, 경도 => 3단어 주소
+     *
+     * @param $lat
+     * @param $lng
+     * @return string
+     */
+    public static function coordinates2Address($lat, $lng)
+    {
+        $query = http_build_query([
+            'coords' => $lat . ',' . $lng,
+            'display' => 'minimal',
+            'key' => static::WHAT_3_WORDS_API_KEY
+        ]);
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, 'https://api.what3words.com/v2/reverse?' . $query);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+
+        $response = curl_exec($ch);
+        $response = json_decode($response, true);
+
+        return $response['words'];
+    }
+}
